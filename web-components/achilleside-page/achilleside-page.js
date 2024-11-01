47baf25039ca68891d7522291ed860b76a04c48f
BiscuitNervos <user@Woland>
2024-11-01 12:34:52 +0200

    Updated to function properly

--- web-components/achilleside-page/achilleside-page.js
+++ web-components/achilleside-page/achilleside-page.js
@@ -1,3 +1,5 @@
+// import sdkModule from "../../../../../../../assistos-sdk/modules/application";
+
 const assistOSSDK = require("assistos")
 const applicationModule = require('assistos').loadModule('application', {});
 
@@ -29,15 +31,22 @@ export class AchillesIDEPage {
             await assistOS.loadifyFunction(async () => {
                 const bookGenerationData = {
                     llm: "ChatGPT",
-                    size: 1500,
+                    size: 8,
                     documentId: this.documentId
                 };
 
+                const formElement = this.element.querySelector("form");
+                const formData = await assistOS.UI.extractFormInformation(formElement);
+                if (!formData.isValid) {
+                    return assistOS.UI.showApplicationError("Invalid form data", "Please fill all the required fields", "error");
+                }
+                const planData = formData.data;
+
                 const response = await applicationModule.runApplicationFlow(
                     assistOS.space.id,
-                    "AchillesIDE",
-                    "GenerateTemplate",
-                    bookGenerationData
+                    "AchillesIDE", // numele aplicatiei
+                    "GenerateTemplate", // numele flow ului
+                    planData
                 );
 
                 const documentId = response.data;
@@ -50,5 +59,10 @@ export class AchillesIDEPage {
             console.error("Error while saving the project:", error);
             alert(`Error: ${error.message || "Unknown error occurred"}`);
         }
+        // try {
+        //     await sdkModule.runApplicationFlow();
+        // } catch (error) {
+        //     console.error("Error in runApplicationFlow:", error);
+        // }
     }
 }
